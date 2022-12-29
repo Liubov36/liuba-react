eaa49ead2ba5d3c4c8203d509d0e67f095c3ee0a
liuba36 <inkkli24@gmail.com>
2022-12-29 19:19:02 +0100

    add files

---
+++ App.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { useCallback } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
+
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+import RegistrationScreen from './Screens/RegistrationScreen';
+import LoginScreen from './Screens/LoginScreen';
+
+const image = require('./assets/images/Photo BG.jpg'); 
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+      <ImageBackground style={styles.image} source={image}>
+      <KeyboardAvoidingView
+          behavior={Platform.OS === 'android' ? 'padding' : 'height'}
+        >
+               <RegistrationScreen />
+              <LoginScreen />
+          </KeyboardAvoidingView>
+        </ImageBackground>
+        <StatusBar style='auto' />
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    justifyContent: 'flex-end',
+  },
+});
